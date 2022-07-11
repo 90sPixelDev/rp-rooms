b19104b951e89303cd2ebcdad9edee1308434f12
90sPixelDev <fontlien@gmail.com>
2022-07-11 03:40:40 -0400

    Started the basic boilerplate of the rp chat project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles/App/App.module.css';
+
+function App() {
+	return (
+		<div className={styles.App}>
+			<h1>Test Title!</h1>
+		</div>
+	);
+}
+
+export default App;
