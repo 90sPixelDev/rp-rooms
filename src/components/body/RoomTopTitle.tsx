afe99b049178bbeb2f0d4f89595587151b9ffb30
90sPixelDev <fontlien@gmail.com>
2022-11-23 02:16:10 -0500

    Added loading anim to room top title

--- src/components/body/RoomTopTitle.tsx
+++ src/components/body/RoomTopTitle.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import loadingAnim from '../../resources/ui/loading-anim.svg';
 
 interface Props {
 	currentChInfo: Record<string, any>;
 }
-// type Props = {
-// 	currentChInfo: Record<string, any>;
-// };
 type Styles = {
 	container: string;
 	roomTitle: string;
+	loading: string;
 };
 
 const RoomTopTitle = (props: Props) => {
@@ -16,7 +15,20 @@ const RoomTopTitle = (props: Props) => {
 		container:
 			'bg-purple-300 h-fit w-[98%] py-1 rounded-b-2xl mx-auto drop-shadow-md shadow-purple-500',
 		roomTitle: 'flow-root m-auto font-bold w-fit min-h-[20px]',
+		loading: 'h-6 m-auto',
 	};
+
+	if (
+		props.currentChInfo.num === '' ||
+		props.currentChInfo.num === null ||
+		props.currentChInfo.num === undefined
+	) {
+		return (
+			<div className={styles.container}>
+				<img className={styles.loading} src={loadingAnim} />
+			</div>
+		);
+	}
 
 	return (
 		<div className={styles.container}>
