4244fc860f788d9c6bfe7ce6c016b99c994bdb66
90sPixelDev <fontlien@gmail.com>
2022-11-25 14:15:09 -0500

    Fixed timestamp error on not showing '0' digit when minutes is less than 10

--- src/components/chat/ChatBox.tsx
+++ src/components/chat/ChatBox.tsx
@@ -40,12 +40,12 @@ const ChatBox = (props: Props) => {
 
 	const getTimeFormat = () => {
 		let hours = date.getHours();
-		let minutes = date.getMinutes();
+		let minutes: string | number = date.getMinutes();
 		const ampm = hours >= 12 ? 'pm' : 'am';
 
 		hours = hours % 12;
 		hours = hours ? hours : 12;
-		minutes = minutes < 10 ? 0 + minutes : minutes;
+		minutes = minutes < 10 ? '0' + minutes : minutes;
 		const strTime = hours + ':' + minutes + ' ' + ampm;
 		return strTime;
 	};
